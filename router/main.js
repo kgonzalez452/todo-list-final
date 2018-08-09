8d9fb934066c5edc44c932805aeaae2969177446
Salvi Pascual <salvi@techlaunch.io>
2018-08-08 19:59:58 -0700

    add save files

--- router/main.js
+++ router/main.js
@@ -1,5 +1,8 @@
-module.exports = function(app, tigers)
+module.exports = function(app, tigers, fs)
 {
+    //
+    // index file
+    //
     app.get('/', function(req, res){
         if(req.session.username) {
             res.render('index', {ptitle:"Tigers", tigers: tigers, username: req.session.username});
@@ -7,6 +10,9 @@ module.exports = function(app, tigers)
         else res.redirect('/login');
     });
 
+    //
+    // add a new tiger
+    //
     app.get('/add', function(req, res){
         if(req.session.username) {
             res.render('add', {ptitle:"Add tiger", username: req.session.username});
@@ -15,10 +21,19 @@ module.exports = function(app, tigers)
     });
 
     app.get('/add-submit', function(req, res){
+        // save tiger to the array
         tigers.push(req.query.name);
+
+        // save tiger to the file
+        fs.writeFile(__dirname+'/../tigers', JSON.stringify(tigers), 'utf8');
+
+        // redirect to home
         res.redirect('/');
     });
 
+    // 
+    // update a tiger
+    //
     app.get('/update', function(req, res){
         if(req.session.username) {
             res.render('update', {ptitle:"Update tiger", username: req.session.username});
@@ -27,10 +42,19 @@ module.exports = function(app, tigers)
     });
 
     app.get('/update-submit', function(req, res){
+        // update tiger array
         tigers.splice(req.query.pos, 1, req.query.name);
+
+        // update tiger from the file
+        fs.writeFile(__dirname+'/../tigers', JSON.stringify(tigers), 'utf8');
+
+        // redirect to home
         res.redirect('/');
     });
 
+    //
+    // delete a tiger
+    //
     app.get('/del', function(req, res){
         if(req.session.username) {
             res.render('delete', {ptitle:"Delete tiger", username: req.session.username});
@@ -39,7 +63,13 @@ module.exports = function(app, tigers)
     });
 
     app.get('/del-submit', function(req, res){
+        // delete from array
         tigers.splice(req.query.pos, 1);
+
+        // delete tiger from the file
+        fs.writeFile(__dirname+'/../tigers', JSON.stringify(tigers), 'utf8');
+
+        // redirect to home
         res.redirect('/');
     });
 
@@ -47,6 +77,9 @@ module.exports = function(app, tigers)
         res.render('login', {ptitle: "Login", hideLogin: true});
     });
 
+    //
+    // login and logout a user
+    //
     app.get('/login-submit', function(req, res){
         req.session.username = req.query.name;
         res.redirect('/');
@@ -57,6 +90,9 @@ module.exports = function(app, tigers)
         res.send('You ended your session correctly');
     });
 
+    //
+    // ajax calls
+    //
     app.get('/get-tigers', function(req, res){
         res.json({tigers: tigers});
     });
--- server.js
+++ server.js
@@ -1,19 +1,37 @@
 var express = require('express');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
+var fs = require('fs');
 var app = express();
 
-var tigers = ['john', 'mathias', 'pepe', 'pedro'];
+// load the tigers
+var tigers = [];
+fs.readFile(__dirname+'/tigers', 'utf8', function(err, data){
+    // load the tigers if there is no error
+    if( ! err) tigers = JSON.parse(data);
 
+    // include routers
+    require('./router/main')(app, tigers, fs);
+
+    // hande 404 errors
+    app.use(function(req, res){
+        res.status(404).render('404');
+    });
+
+    // handle generic errors
+    app.use(function(err, req, res, next){
+        res.status(500).send('The app found an error. Please try again later');
+    });
+});
+
+// initialize the cookies and session
 app.use(cookieParser());
 app.use(session({secret: 'qwerty'}));
 
-require('./router/main')(app, tigers);
-
+// initialize the views using ejs
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-var server = app.listen(8080, function(){
-    console.log('hello world');
-});
+// start the server
+var server = app.listen(8080);
